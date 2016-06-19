ddd555e211b416262839edbdbb551bace19bd10d
Humza Haider <haide043@morris.umn.edu>
2016-06-19 16:19:38 -0500

    Started initial upload and variable selection process. Also saved example data for testing purposes. Data is from: "http://www.ats.ucla.edu/stat/data/binary.csv"

--- server.R
+++ server.R
@@ -1,1 +1,26 @@
 #server.R
+
+shinyServer(function(input, output) {
+  
+  values <- reactiveValues(dataset = NULL)
+  
+  observeEvent(input$file, {
+    values$dataset <- read.csv(input$file$datapath)
+  })
+  
+  output$dependent <- renderUI({
+    deps = names(values$dataset)
+    validate(need(!is.null(deps), "Please upload a file to see more options."))
+
+    radioButtons("dependent", "Choose your dependent variable.", deps)
+  })
+  
+  output$independent <- renderUI({
+    remove = input$dependent
+    indeps = setdiff(names(values$dataset),remove)
+    
+    validate(need(!is.null(indeps), ""))
+    
+    checkboxGroupInput("independent", "Choose your independent variables.", indeps)
+  })
+})
--- ui.R
+++ ui.R
@@ -1,1 +1,21 @@
 #ui.R
+
+library(shiny)
+library(ggplot2)
+
+shinyUI(fluidPage(
+  
+  title = "Interactive Logistic Regression",
+  
+  fluidRow(
+    column(3,
+           h2("Interactive Logistic Regression"),
+           fileInput("file", "Upload file"),
+           
+           uiOutput("dependent"),
+           uiOutput("independent")
+           
+           
+    )
+  )
+))
